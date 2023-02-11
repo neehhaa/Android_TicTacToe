b48096f68ce74e5a924096fedd24a5c51795f0f1
neehhaa <nehawasnik95@outlook.com>
2023-02-11 16:24:06 +0530

    basic tictactoe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    String b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    int flag,count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,56 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.btn7);
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
+    }
+    public void Check(View view){
+        Button btnCurrent = (Button) view;
+        count++;
+
+        if (flag == 0){
+            btnCurrent.setText("X");
+            flag = 1;
+        } else {
+            btnCurrent.setText("0");
+            flag = 0;
+        }
+        if (count > 4){
+            b1 = btn1.getText().toString();
+            b2 = btn2.getText().toString();
+            b3 = btn3.getText().toString();
+            b4 = btn4.getText().toString();
+            b5 = btn5.getText().toString();
+            b6 = btn6.getText().toString();
+            b7 = btn7.getText().toString();
+            b8 = btn8.getText().toString();
+            b9 = btn9.getText().toString();
+
+            //Conditions
+
+            if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")){
+
+                Toast.makeText(this,"WINNER IS "+b1,Toast.LENGTH_SHORT).show();
+            } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
+                //2
+                Toast.makeText(this,"WINNER IS "+b4,Toast.LENGTH_SHORT).show();
+            } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
+                //3
+                Toast.makeText(this,"WINNER IS "+b7,Toast.LENGTH_SHORT).show();
+            } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
+                //4
+                Toast.makeText(this,"WINNER IS "+b1,Toast.LENGTH_SHORT).show();
+
+            }else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
+                Toast.makeText(this,"WINNER IS "+b2,Toast.LENGTH_SHORT).show();
+
+            }else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
+                Toast.makeText(this,"WINNER IS "+b3,Toast.LENGTH_SHORT).show();
+
+            }else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
+                Toast.makeText(this,"WINNER IS "+b1,Toast.LENGTH_SHORT).show();
+
+            }else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
+                Toast.makeText(this,"WINNER IS "+b3,Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
