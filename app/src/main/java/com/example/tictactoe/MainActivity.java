fd7f22989ab3f21d4140a2184cbbdcf7fc892ad4
neehhaa <nehawasnik95@outlook.com>
2023-02-11 22:11:55 +0530

    restart button

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,restart;
     String b1, b2, b3, b4, b5, b6, b7, b8, b9;
     int flag,count = 0;
 
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newGame();
+            }
+        });
 
     }
     private void init(){
@@ -31,56 +37,76 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.btn7);
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
+        restart = findViewById(R.id.restart);
     }
-    public void Check(View view){
+    public void Check(View view) {
         Button btnCurrent = (Button) view;
-        count++;
 
-        if (flag == 0){
-            btnCurrent.setText("X");
-            flag = 1;
-        } else {
-            btnCurrent.setText("0");
-            flag = 0;
-        }
-        if (count > 4){
-            b1 = btn1.getText().toString();
-            b2 = btn2.getText().toString();
-            b3 = btn3.getText().toString();
-            b4 = btn4.getText().toString();
-            b5 = btn5.getText().toString();
-            b6 = btn6.getText().toString();
-            b7 = btn7.getText().toString();
-            b8 = btn8.getText().toString();
-            b9 = btn9.getText().toString();
+        if (btnCurrent.getText().toString().equals("")) {
+            count++;
 
-            //Conditions
+            if (flag == 0) {
+                btnCurrent.setText("X");
+                flag = 1;
+            } else {
+                btnCurrent.setText("0");
+                flag = 0;
+            }
+            if (count > 4) {
+                b1 = btn1.getText().toString();
+                b2 = btn2.getText().toString();
+                b3 = btn3.getText().toString();
+                b4 = btn4.getText().toString();
+                b5 = btn5.getText().toString();
+                b6 = btn6.getText().toString();
+                b7 = btn7.getText().toString();
+                b8 = btn8.getText().toString();
+                b9 = btn9.getText().toString();
 
-            if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")){
+                //Conditions
 
-                Toast.makeText(this,"WINNER IS "+b1,Toast.LENGTH_SHORT).show();
-            } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
-                //2
-                Toast.makeText(this,"WINNER IS "+b4,Toast.LENGTH_SHORT).show();
-            } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
-                //3
-                Toast.makeText(this,"WINNER IS "+b7,Toast.LENGTH_SHORT).show();
-            } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
-                //4
-                Toast.makeText(this,"WINNER IS "+b1,Toast.LENGTH_SHORT).show();
-
-            }else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
-                Toast.makeText(this,"WINNER IS "+b2,Toast.LENGTH_SHORT).show();
-
-            }else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
-                Toast.makeText(this,"WINNER IS "+b3,Toast.LENGTH_SHORT).show();
-
-            }else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
-                Toast.makeText(this,"WINNER IS "+b1,Toast.LENGTH_SHORT).show();
-
-            }else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
-                Toast.makeText(this,"WINNER IS "+b3,Toast.LENGTH_SHORT).show();
+                if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b1, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b4, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b7, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b1, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b2, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b3, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b1, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
+                    Toast.makeText(this, "WINNER IS " + b3, Toast.LENGTH_SHORT).show();
+                    newGame();
+                }else if (count == 9){
+                    Toast.makeText(this,"Game is Drawn",Toast.LENGTH_SHORT).show();
+                    newGame();
+                }
             }
         }
+    }
+    public void newGame(){
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+        btn9.setText("");
+        count = 0;
+        flag = 0;
     }
 }
